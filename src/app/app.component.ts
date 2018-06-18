2d48b82572a52168add400c992f2a43d63b119b7
Zhi Li <zhi.li@invoicefair.com>
2018-06-18 14:46:16 +0100

    decorators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,30 @@ function log(target, name, descriptor) {
 
 	descriptor.value = function(...args) {
 		console.log("args: ", args)
+		const result = original.apply(this, args)
+		console.log("the result is: ", result)
 	}
 
 	return descriptor
 }
+
+function log1(className) {
+	console.log(className)
+
+	return (...args) => {
+		console.log("args passed to this class are", args)
+		return new className(...args)
+	}
+}
+
+@log1
+class myExampleClass {
+	constructor(arg1, arg2) {
+		console.log("constructor fired")
+	}
+}
+
+const myClass = new myExampleClass(5, 10)
 
 @Component({
 	selector: 'app-root',
